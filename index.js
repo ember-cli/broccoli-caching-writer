05f987d9bd7766970eb24ecfc01b8e6ed84b5f55
Robert Jackson <robert.w.jackson@me.com>
2014-04-27 21:44:45 -0400

    Create a relativeKeysForTree helper.
    The version that exists in broccoli-kitchen-sink-helpers includes both
    the original (root) directory `basename` as well as directory `mtimes`.
    
    The issue with `mtimes` is that the `copyRecursiveSync` function
    preserves them for files, but we are using `mkdirp` to create the
    directories, and the `mtime`'s no longer match.

--- index.js
+++ index.js
@@ -33,11 +33,14 @@ CachingWriter.prototype.write = function (readTree, destDir) {
   var self = this
 
   return readTree(this.inputTree).then(function (srcDir) {
-    var inputTreeHash = helpers.hashTree(srcDir);
+    var inputTreeKeys = relativeKeysForTree(srcDir);
+    var inputTreeHash = helpers.hashStrings(inputTreeKeys);
 
     if (inputTreeHash !== self._cacheHash) {
       self.updateCache(srcDir, self.getCleanCacheDir());
-      self._cacheHash = inputTreeHash;
+
+      self._cacheHash     = inputTreeHash;
+      self._cacheTreeKeys = inputTreeKeys;
     }
 
     helpers.copyRecursivelySync(self.getCacheDir(), destDir);
@@ -54,3 +57,61 @@ CachingWriter.prototype.updateCache = function (srcDir, destDir) {
 }
 
 module.exports = CachingWriter;
+
+function relativeKeysForTree (root, relativePath, _stack, _followSymlink) {
+  var stats, statKeys
+  if (!relativePath) { relativePath = ''; }
+  var fullPath = path.join(root, relativePath);
+  try {
+    if (_followSymlink) {
+      stats = fs.statSync(fullPath)
+    } else {
+      stats = fs.lstatSync(fullPath)
+    }
+  } catch (err) {
+    console.warn('Warning: failed to stat ' + fullPath)
+    // fullPath has probably ceased to exist. Leave `stats` undefined and
+    // proceed hashing.
+  }
+  var childKeys = []
+  if (stats && stats.isDirectory()) {
+    var fileIdentity = stats.dev + '\x00' + stats.ino
+    if (_stack != null && _stack.indexOf(fileIdentity) !== -1) {
+      console.warn('Symlink directory loop detected at ' + fullPath + ' (note: loop detection may have false positives on Windows)')
+    } else {
+      if (_stack != null) _stack = _stack.concat([fileIdentity])
+      var entries
+      try {
+        entries = fs.readdirSync(fullPath).sort()
+      } catch (err) {
+        console.warn('Warning: Failed to read directory ' + fullPath)
+        console.warn(err.stack)
+        childKeys = ['readdir failed']
+        // That's all there is to say about this directory.
+      }
+      if (entries != null) {
+        for (var i = 0; i < entries.length; i++) {
+          childKeys = childKeys.concat(relativeKeysForTree(root, path.join(relativePath, entries[i]), _stack))
+        }
+      }
+    }
+    statKeys = ['dir - stats', stats.mode, stats.size];
+  } else if (stats && stats.isSymbolicLink()) {
+    if (_stack == null) {
+      // From here on in the traversal, we need to guard against symlink
+      // directory loops. _stack is kept null in the absence of symlinks to we
+      // don't have to deal with Windows for now, as long as it doesn't use
+      // symlinks.
+      _stack = []
+    }
+    childKeys = relativeKeysForTree(root, relativePath, _stack, true) // follow symlink
+  }
+
+  if (!statKeys) {
+    statKeys = ['stats', stats.mode, stats.size, stats.mtime.getTime()];
+  }
+  // Perhaps we should not use basename to infer the file name
+  return ['path', path.basename(relativePath)]
+    .concat(stats ? statKeys : ['stat failed'])
+    .concat(childKeys)
+}
