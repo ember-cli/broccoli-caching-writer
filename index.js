77baf111c0b71dc4b704781eab47d6f2a500c9a0
Stefan Penner <stefan.penner@gmail.com>
2015-02-19 11:30:51 -0800

    extract key to its own file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var generateRandomString = require('./lib/generate-random-string');
 var assign = require('lodash-node/modern/object/assign');
 var CoreObject = require('core-object');
 var debugGenerator = require('debug');
+var Key = require('./key');
 
 var proto = {};
 
@@ -38,7 +39,6 @@ proto.init = function(inputTrees, options) {
   } else {
     this.inputTrees = [inputTrees];
   }
-
 
   if (this.filterFromCache === undefined) {
     this.filterFromCache = {};
@@ -164,75 +164,6 @@ proto.shouldBeIgnored = function (fullPath) {
 
   // Otherwise, don't ignore this file
   return (this._shouldBeIgnoredCache[fullPath] = false);
-};
-
-var EMPTY_ARRAY;
-function Key(type, fullPath, path, stat, children, debug) {
-  this.type = type;
-  this.fullPath = fullPath;
-  this.path = path;
-  this.stat = stat;
-  this.children = children || EMPTY_ARRAY;
-  this.debug = debug;
-}
-
-Key.prototype.toString = function() {
-  return ' type: '       + this.type +
-         ' fullPath: '   + this.fullPath +
-         ' path: '       + this.path +
-         ' stat.mode: '  + this.stat.mode +
-         ' stat.size: '  + this.stat.size +
-         ' stat.mtime: ' + this.stat.mtime.getTime();
-};
-
-Key.prototype.inspect = function() {
-  return [
-    this.type,
-    this.path,
-    this.stat.mode,
-    this.stat.size,
-    this.stat.size,
-    this.stat.mtime.getTime()
-  ].join(', ');
-};
-
-function logNotEqual(previous, next) {
-  previous.debug(" cache eviction due to: \n     - {%o} \n     - {%o}", previous, next);
-}
-
-Key.prototype.equal = function(otherKey) {
-  if (otherKey === undefined) {
-    logNotEqual(this, otherKey);
-    return false;
-  }
-
-  if (this.type === otherKey.type && this.type === 'directory') {
-    var children = this.children;
-    var otherChildren = otherKey.children;
-
-    if (children.length === otherChildren.length) {
-      for (var i = 0; i < children.length; i++) {
-        if (children[i].equal(otherChildren[i])) {
-          // they are the same
-        } else {
-          return false;
-        }
-      }
-    } else {
-      return false;
-    }
-  }
-
-  // key represents a file, diff the file
-  if (this.type       === otherKey.type &&
-      this.path       === otherKey.path &&
-      this.stat.mode  === otherKey.stat.mode &&
-      this.stat.size  === otherKey.stat.size &&
-      this.type === 'directory' ? true : this.stat.mtime.getTime() === otherKey.stat.mtime.getTime()) {
-    return true;
-  } else {
-    logNotEqual(this, otherKey);
-  }
 };
 
 proto.keyForTree = function (fullPath, initialRelativePath) {
