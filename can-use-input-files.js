24a792ab7b46dac34a9034612eab965971771a5c
Stefan Penner <stefan.penner@gmail.com>
2015-08-05 23:12:56 -0700

    fix CC

--- can-use-input-files.js
+++ can-use-input-files.js
@@ -2,6 +2,6 @@ module.exports = function canUseInputFiles(inputFiles) {
   if (!Array.isArray(inputFiles)) { return false; }
 
   return inputFiles.filter(function(file) {
-    return !/[\{\}\*]/.test(file);
+    return !/[\{\}\|\*\?\!]/.test(file);
   }).length === inputFiles.length;
 };
